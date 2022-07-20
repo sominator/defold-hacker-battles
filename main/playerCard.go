2ee4209c3cd5ff1b3ab3a6130c8e28d95f92d6a1
Som Pourfarzaneh <doryoku@gmail.com>
2022-07-20 15:14:09 -0700

    Working on drag/drop functionality with dropzone.

--- main/playerCard.go
+++ main/playerCard.go
@@ -1,3 +1,18 @@
+components {
+  id: "card"
+  component: "/main/card.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
@@ -49,7 +64,7 @@ embedded_components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/main/cyanCardAtlas.atlas\"\n"
+  data: "tile_set: \"/main/playerCardAtlas.atlas\"\n"
   "default_animation: \"Cyan_Ping3x\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
